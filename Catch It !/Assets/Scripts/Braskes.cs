94aa573ecf5b8d3a43e40ac9da23505f3ee0b2f5
EmilisSaltys <47056599+EmilisSaltys@users.noreply.github.com>
2019-02-19 14:08:16 +0200

    Game

---
+++ Catch It !/Assets/Scripts/Braskes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Braskes : MonoBehaviour
+{
+
+    public GameObject braske;
+
+    public Transform FirePoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        Instantiate(braske, FirePoint.position, FirePoint.rotation);
+    }
+
+}
---
+++ Catch It !/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveRight();
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+                MoveLeft();
+        }
+    }
+
+   public void MoveRight()
+    {
+        rb.velocity = new Vector2(8, 0);
+    }
+
+   public void MoveLeft()
+    {
+        rb.velocity = new Vector2(-8, 0);
+    }
+}
